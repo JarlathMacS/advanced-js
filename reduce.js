3b82dbdf2c6a96ce4c50a8469bbf94e9f56e103f
Jarlath <jarlath@jarlath.cc>
2025-04-15 21:35:05 -0500

    Add reduce part 2 examples

--- reduce.js
+++ reduce.js
@@ -32,5 +32,23 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
+
 
 // Grouping by a property, and totaling it too
+// {Developer: 12, Designer: 4} <-- this is what we want!
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+    let key = curr.profession;
+    if (!acc[key]) {
+        acc[key] = curr.yrsExperience;
+    } else {
+        acc[key] += curr.yrsExperience;
+    }
+    // console.log(key);
+    // console.log(acc[key]);
+    // console.log(acc);
+    return acc;    
+}, {});
+
+console.log(experienceByProfession);
